acb7acbe2b7991d85456ac6a738b1c8445b474a8
David Burke <burkedm94@gmail.com>
2025-01-30 12:12:46 -0500

    Add item search by text and tag

--- Assets/Scripts/Guild Management/Stockpile Page/StockpilePage.cs
+++ Assets/Scripts/Guild Management/Stockpile Page/StockpilePage.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,20 +11,36 @@ public class StockpilePage : MonoBehaviour
     public GameObject itemList;
     public GameObject itemListItemPrefab;
     public Item selectedItem;
+    public TMP_InputField inputField;
+    public bool searchItemsByName = true;
+    public bool searchItemsByTag = true;
 
     private void OnEnable()
     {
         ResetList();
     }
 
-    void ResetList()
+    void ResetList(string searchText = "", bool searchByName = false, bool searchByTag = false)
     {
+        if (GuildManager.instance == null || GuildManager.instance.stockpile == null)
+        {
+            return;
+        }
         GuildManager.instance.stockpile.RemoveAll(item => item.quantity == 0);
         foreach (Transform child in itemList.transform)
         {
             Destroy(child.gameObject);
         }
-        foreach (Item item in GuildManager.instance.stockpile)
+        List<Item> items = new List<Item>();
+        if (searchText == "")
+        {
+            items = GuildManager.instance.stockpile;
+        }
+        if (searchText != "")
+        {
+            items = GuildManager.instance.stockpile.Where(i => CheckNameFit(i, searchText, searchByName, searchByTag)).ToList();
+        }
+        foreach (Item item in items)
         {
             ItemListItem itemListItem = Instantiate(itemListItemPrefab, itemList.transform).GetComponent<ItemListItem>();
             Button button = itemListItem.gameObject.GetComponent<Button>();
@@ -33,7 +52,35 @@ public class StockpilePage : MonoBehaviour
         }
     }
 
-    void SelectItem(Item item)
+    private bool CheckNameFit(Item item, string searchText, bool searchByName, bool searchByTag)
+    {
+        string search = searchText.ToLower();
+        if (searchByName)
+        {
+            if (item.itemName.ToLower().Contains(search)) {
+                return true;
+            }
+        }
+        if (searchByTag)
+        {
+            if (item.tags.Any(t => t.ToLower().Contains(search)))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void HandleTextFieldEntry()
+    {
+        if (inputField.text == "")
+        {
+            ResetList();
+        }
+        ResetList(inputField.text, searchItemsByName, searchItemsByTag);
+    }
+
+    public void SelectItem(Item item)
     {
         selectedItem = item;
     }
