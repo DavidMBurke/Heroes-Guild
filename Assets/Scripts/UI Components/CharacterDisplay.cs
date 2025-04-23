10593a67be622a0ab49167d68921c9b0fcffee33
David Burke <burkedm94@gmail.com>
2025-04-23 12:45:51 -0400

    Select and deselect characters for movement group

--- Assets/Scripts/UI Components/CharacterDisplay.cs
+++ Assets/Scripts/UI Components/CharacterDisplay.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// Display for individual characters in the party bar
@@ -17,6 +18,9 @@ public class CharacterDisplay : MonoBehaviour
     private GameObject actionPointsObject = null!;
     private HealthBar healthBar = null!;
     private ActionManager actionManager = null!;
+    public Outline outline;
+    public Color movementGroupColor;
+    public Color selectedColor;
     
     private void Start()
     {
@@ -42,6 +46,18 @@ public class CharacterDisplay : MonoBehaviour
         characterName.text = character.characterName;
         actionPoints.text = character.actionPoints.ToString();
         healthBar.UpdateHealthBar(character.health, character.maxHealth);
+        if (PartyManager.instance.movementGroup.Contains(character))
+        {
+            outline.enabled = true;
+            outline.effectColor = movementGroupColor;
+        } else if (character == actionManager.currentBeing)
+        {
+            outline.enabled = true;
+            outline.effectColor = selectedColor;
+        } else
+        {
+            outline.enabled = false;
+        }
     }
 
     public void OnClick()
@@ -53,7 +69,10 @@ public class CharacterDisplay : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
         {
-            partyManager.AddOrRemoveCharacterFromMovementGroup(character);
+            if (character != actionManager.currentBeing)
+            {
+                partyManager.AddOrRemoveCharacterFromMovementGroup(character);
+            }
         } 
         else
         {
